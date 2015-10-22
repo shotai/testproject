b06c5d3f9301649878e0e41e90bf7f3553f8768d
Mg <gamem.8818@gmail.com>
2015-10-22 16:41:37 -0400

    register update

--- metadatarequest.py
+++ metadatarequest.py
@@ -7,47 +7,66 @@ import service
 class MetadataRequest:
     @staticmethod
     def get_host():
-        res = requests.get("http://rancher-metadata/2015-07-25/self/host", header={"Accept":"application/json"})
-        res.json()
-        print(res)
+        res = requests.get(url="http://rancher-metadata/2015-07-25/self/host", headers={"Accept": "application/json"})
+        res = res.json()
         tmp_host = host.Host()
         tmp_host.agent_ip = res['agent_ip']
         tmp_host.name = res['name']
-        tmp_host.uuid = res['uuid']
-        for k,v in res['labels'].items():
-            tmp_host.add_labels(k, v)
+        tmp_host.labels = res['labels']
         return tmp_host
 
     @staticmethod
     def get_stack():
-        res = requests.get("http://rancher-metadata/2015-07-25/self/stack", header={"Accept":"application/json"})
-        res.json()
-        print(res)
-        tmp_stack = stack.stack()
+        res = requests.get(url="http://rancher-metadata/2015-07-25/self/stack",
+                           headers={"Accept": "application/json"})
+        res = res.json()
+        tmp_stack = stack.Stack()
         tmp_stack.name = res['name']
         tmp_stack.environment_name = res['environment_name']
-        for i in res['services']:
-            tmp_stack.add_service(i)
+        tmp_stack.services = res['services']
         return tmp_stack
 
     @staticmethod
-    def get_service(name):
-        res = requests.get("http://rancher-metadata/2015-07-25/services/"+name, header={"Accept":"application/json"})
-        res.json()
+    def get_other_service(name):
+        res = requests.get(url="http://rancher-metadata/2015-07-25/services/"+name,
+                           headers={"Accept": "application/json"})
+        res = res.json()
         print(res)
         tmp_service = service.Service()
         tmp_service.name = res['name']
         tmp_service.hostname = res['hostname']
         tmp_service.metadata = res['metadata']
         tmp_service.kind = res['kind']
+        tmp_service.stack_name = res['stack_name']
+        tmp_service.ports = res['ports']
+        tmp_service.labels = res['labels']
+        tmp_service.links = res['links']
+        # for k, v in res['links'].items():
+        #     tmp_service.links.append(k.split("/")[1])
 
-        for i in res['ports']:
-            tmp_service.add_port(i)
-        for k,v in res['labels'].items():
-            tmp_service.add_labels(k, v)
+        return tmp_service
 
-        for k,v in res['labels'].items():
-            tmp_service.add_labels(k, v)
+    @staticmethod
+    def get_self_service():
+        res = requests.get(url="http://rancher-metadata/2015-07-25/self/service",
+                           headers={"Accept": "application/json"})
+        res = res.json()
+        print(res)
+        tmp_service = service.Service()
+        tmp_service.name = res['name']
+        tmp_service.hostname = res['hostname']
+        tmp_service.metadata = res['metadata']
+        tmp_service.kind = res['kind']
+        tmp_service.stack_name = res['stack_name']
+        tmp_service.ports = res['ports']
+        tmp_service.labels = res['labels']
+        tmp_service.links = res['links']
+        # for k, v in res['links'].items():
+        #     tmp_service.links.append(k.split("/")[1])
+
+        return tmp_service
+
+
 
 
 
