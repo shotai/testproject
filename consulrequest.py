2d17a013293f4a153e7ed8ad2331ef67120bf470
Mg <gamem.8818@gmail.com>
2015-10-26 12:32:44 -0700

    add agent

--- consulrequest.py
+++ consulrequest.py
@@ -21,11 +21,31 @@ class ConsulRequest:
         print("register_service: " + service.stack_name+'/'+service.name + "result: " + r.text)
 
     @staticmethod
+    def register_agent_service(service, host, consul_url):
+        payload = {
+                   "ID": service.stack_name+'/'+service.name,
+                   "Name": service.name,
+                   "Tags": service.tags,
+                   "Address": host.agent_ip,
+                   "Port": int(host.port)
+                   }
+        url = consul_url + "/v1/agent/service/register"
+        r = requests.post(url, json=payload)
+        service.print_service()
+        print("register_service: " + service.stack_name+'/'+service.name + "result: " + r.text)
+
+    @staticmethod
     def deregister_service(service_id, host, consul_url):
         payload = {"Datacenter": host.dc,
                    "Node": host.name,
                    "ServiceID": service_id}
         url = consul_url + "/v1/catalog/deregister"
         r = requests.post(url, json=payload)
+        print("deregister_service: " + service_id + "result: " + r.text)
+
+    @staticmethod
+    def deregister_agent_service(service_id, consul_url):
+        url = consul_url + "/v1/agent/service/deregister/"+service_id
+        r = requests.post(url)
         print("deregister_service: " + service_id + "result: " + r.text)
 
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import threading
 import time
 
 
-def start_register(gateway_services_name, register_host, consul_url):
+def start_label_register(gateway_services_name, register_host, consul_url):
     curr_registered_services = []
     while True:
         register_services = []
@@ -30,7 +30,7 @@ def start_register(gateway_services_name, register_host, consul_url):
         time.sleep(5)
 
 
-def start_register2(gateway_services_name, register_host, consul_url):
+def start_link_register(gateway_services_name, register_host, consul_url):
     curr_registered_services = []
     while True:
         register_services = []
@@ -46,7 +46,28 @@ def start_register2(gateway_services_name, register_host, consul_url):
 
         for i in curr_registered_services:
             if i not in register_services:
-                consulrequest.ConsulRequest.deregister_service(i, register_host, consul_url)
+                consulrequest.ConsulRequest.deregister_service(i, consul_url)
+        curr_registered_services = register_services
+
+        time.sleep(5)
+
+def start_agent_register(gateway_services_name, register_host, consul_url):
+    curr_registered_services = []
+    while True:
+        register_services = []
+        gateway_service = metadatarequest.MetadataRequest.get_other_service(gateway_services_name)
+        for link_service in gateway_service.links:
+            tmp_service_name = gateway_service.stack_name+'/'+link_service
+            if tmp_service_name not in curr_registered_services:
+                tmp_service = metadatarequest.MetadataRequest.get_other_service(link_service)
+                for loc in tmp_service.labels['location'].split(","):		
+                    tmp_service.tags.append(loc)
+                    consulrequest.ConsulRequest.register_agent_service(tmp_service, register_host, consul_url)
+            register_services.append(tmp_service_name)
+
+        for i in curr_registered_services:
+            if i not in register_services:
+                consulrequest.ConsulRequest.deregister_agent_service(i, consul_url)
         curr_registered_services = register_services
 
         time.sleep(5)
@@ -55,13 +76,19 @@ def start_register2(gateway_services_name, register_host, consul_url):
 def main():
     consul_url = os.environ.get("CONSUL", "http://localhost:8500")
     data_center = os.environ.get("DATACENTER", "dc1")
+    register_mode = os.environ.get("MODE", "agent")
+    mode_switcher = {
+        "agent": start_agent_register,
+        "link": start_link_register,
+        "label": start_label_register}
     gateway_services_name = metadatarequest.MetadataRequest.get_self_service().links[0]
     gateway_service = metadatarequest.MetadataRequest.get_other_service(gateway_services_name)
     register_host = metadatarequest.MetadataRequest.get_host()
     register_host.port = gateway_service.ports[0].split(":")[0]
     register_host.dc = data_center
-
-    d = threading.Thread(name='daemon', target=start_register2(gateway_services_name, register_host, consul_url))
+    
+    mode = mode_switcher.get(register_mode, start_agent_register) 
+    d = threading.Thread(name='daemon', target=mode(gateway_services_name, register_host, consul_url))
     d.setDaemon(True)
     d.start()
 
