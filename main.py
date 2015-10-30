5f26706eef8122d6627c96e4a5117b6456744767
Mg <gamem.8818@gmail.com>
2015-10-30 13:56:24 -0400

    update main

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def start_label_register(gateway_services_name, register_host, consul_url):
         gateway_service = metadatarequest.MetadataRequest.get_other_service(gateway_services_name)
         for label_key, label_value in gateway_service.labels.items():
             if label_key.startswith("io.rancher.loadbalancer.target"):
-                tmp_service_name = gateway_service.stack_name+'/'+label_key.split(".")[-1]
+                tmp_service_name = gateway_service.stack_name + '/' + label_key.split(".")[-1]
                 if tmp_service_name not in curr_registered_services:
                     tmp_service = service.Service()
                     tmp_service.name = label_key.split(".")[-1]
@@ -32,72 +32,61 @@ def start_label_register(gateway_services_name, register_host, consul_url):
 
 
 def start_remote_link_register(gateway_services_name, register_host, consul_url):
-    curr_registered_services = []
     print("Start remote link register")
+    start_link_register(gateway_services_name, register_host, consul_url, True)
+
+
+def start_agent_link_register(gateway_services_name, register_host, consul_url):
+    print("Start agent link register")
+    start_link_register(gateway_services_name, register_host, consul_url, False)
+
+
+def start_link_register(gateway_services_name, register_host, consul_url, is_remote):
+    curr_registered_services = []
+    if is_remote:
+        register_method = consulrequest.ConsulRequest.remote_register_service
+        deregister_method = consulrequest.ConsulRequest.remote_deregister_service
+    else:
+        register_method = consulrequest.ConsulRequest.agent_register_service
+        deregister_method = consulrequest.ConsulRequest.agent_deregister_service
+
     while True:
         register_services = []
         gateway_service = metadatarequest.MetadataRequest.get_other_service(gateway_services_name)
         for link_service in gateway_service.links:
-            tmp_service_name = gateway_service.stack_name+'/'+link_service
+            tmp_service_name = gateway_service.stack_name + '/' + link_service
             if tmp_service_name not in curr_registered_services:
                 tmp_service = metadatarequest.MetadataRequest.get_other_service(link_service)
                 try:
                     for loc in tmp_service.labels['location'].split(","):
                         tmp_service.tags.append(loc)
-                        consulrequest.ConsulRequest.remote_register_service(tmp_service, register_host, consul_url)
+                        register_method(tmp_service, register_host, consul_url)
                 except KeyError:
-                    print("Missing location label in "+link_service)
+                    print("Missing location label in " + link_service)
                     pass
             register_services.append(tmp_service_name)
 
         for i in curr_registered_services:
             if i not in register_services:
-                consulrequest.ConsulRequest.remote_deregister_service(i, register_host, consul_url)
+                deregister_method(i, register_host, consul_url)
         curr_registered_services = register_services
 
         time.sleep(5)
+        
 
-
-def start_agent_link_register(gateway_services_name, register_host, consul_url):
-    curr_registered_services = []
-    print("start agent link register")
-    while True:
-        register_services = []
-        gateway_service = metadatarequest.MetadataRequest.get_other_service(gateway_services_name)
-        for link_service in gateway_service.links:
-            tmp_service_full_name = gateway_service.stack_name+'/'+link_service
-            if tmp_service_full_name not in curr_registered_services:
-                tmp_service = metadatarequest.MetadataRequest.get_other_service(link_service)
-                try:
-                    for loc in tmp_service.labels['location'].split(","):
-                        tmp_service.tags.append(loc)
-                        consulrequest.ConsulRequest.agent_register_service(tmp_service, register_host, consul_url)
-                except KeyError:
-                    print("Missing location label in "+link_service)
-                    pass
-            register_services.append(tmp_service_full_name)
-
-        for i in curr_registered_services:
-            if i not in register_services:
-                consulrequest.ConsulRequest.agent_deregister_service(i, consul_url)
-        curr_registered_services = register_services
-
-        time.sleep(5)
-
-
-def start_tcp_agent_register(register_host, consul_url):
+def start_tcp_agent_register(gateway_services_name, register_host, consul_url):
     curr_registered_services = []
     while True:
         register_services = []
         tmp_stack = metadatarequest.MetadataRequest.get_self_stack()
         for stack_service in tmp_stack.services:
             tmp_service = metadatarequest.MetadataRequest.get_other_service(stack_service)
-            tmp_service_full_name = tmp_service.stack_name+"/"+stack_service
+            tmp_service_full_name = tmp_service.stack_name + "/" + stack_service
             try:
                 ports = tmp_service.labels['tcpport']
                 register_services.append(tmp_service_full_name)
             except KeyError:
-                print("No tcpport label in "+stack_service)
+                print("No tcpport label in " + stack_service)
                 continue
             if tmp_service_full_name not in curr_registered_services:
                 for port in ports.split(","):
@@ -109,9 +98,34 @@ def start_tcp_agent_register(register_host, consul_url):
                 consulrequest.ConsulRequest.agent_deregister_service(i, consul_url)
         curr_registered_services = register_services
 
+        time.sleep(5)
 
 
-    return
+def start_tcp_link_agent_register(gateway_services_name, register_host, consul_url):
+    curr_registered_services = []
+    while True:
+        register_services = []
+        self_service = metadatarequest.MetadataRequest.get_self_service()
+        for stack_service in self_service.links:
+            tmp_service = metadatarequest.MetadataRequest.get_other_service(stack_service)
+            tmp_service_full_name = tmp_service.stack_name + "/" + stack_service
+            try:
+                ports = tmp_service.labels['tcpport']
+                register_services.append(tmp_service_full_name)
+            except KeyError:
+                print("No tcpport label in " + stack_service)
+                continue
+            if tmp_service_full_name not in curr_registered_services:
+                for port in ports.split(","):
+                    tmp_service.tags.append(port)
+                    consulrequest.ConsulRequest.agent_register_service(tmp_service, register_host, consul_url)
+
+        for i in curr_registered_services:
+            if i not in register_services:
+                consulrequest.ConsulRequest.agent_deregister_service(i, consul_url)
+        curr_registered_services = register_services
+
+        time.sleep(5)
 
 
 def main():
@@ -124,32 +138,21 @@ def main():
         "remoteLink": start_remote_link_register,
         "label": start_label_register,
         "agentTcp": start_tcp_agent_register}
-    #gateway_services_name = metadatarequest.MetadataRequest.get_self_service().links[0]
+    # gateway_services_name = metadatarequest.MetadataRequest.get_self_service().links[0]
     gateway_service = metadatarequest.MetadataRequest.get_other_service(gateway_services_name)
     register_host = metadatarequest.MetadataRequest.get_host()
     register_host.port = gateway_service.ports[0].split(":")[0]
     register_host.dc = data_center
-    
-    mode = mode_switcher.get(register_mode, start_agent_link_register)
-    d = threading.Thread(name='daemon', target=mode(gateway_services_name, register_host, consul_url))
-    d.setDaemon(True)
-    d.start()
+
+    for i in register_mode.split(","):
+
+        mode = mode_switcher.get(register_mode, start_agent_link_register)
+    threading.Thread(name=register_mode,
+                     target=mode(gateway_services_name, register_host, consul_url),
+                     daemon=True).start()
 
     v = input("press any key to exit.")
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
