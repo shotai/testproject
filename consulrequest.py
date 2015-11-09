df70601aca4dc8f715d3b63c9b33387cb85a1f75
Mg <gamem.8818@gmail.com>
2015-11-09 17:25:35 -0500

    cleanup

--- consulrequest.py
+++ consulrequest.py
@@ -4,47 +4,6 @@ import requests
 class ConsulRequest:
 
     @staticmethod
-    def remote_register_service(service, host, consul_url):
-        payload = {
-                    "Datacenter": host.dc,
-                    "Node": host.name,
-                    "Address": host.agent_ip,
-                    "Service": {
-                        "ID": service.stack_name+'/'+service.name,
-                        "Service": service.name,
-                        "Tags": service.tags,
-                        "Address": host.agent_ip,
-                        "Port": int(host.port)
-                    }
-                }
-        url = consul_url + "/v1/catalog/register"
-        r = requests.post(url, json=payload)
-        service.print_service()
-        print("Remote Register Service: " + service.stack_name+'/'+service.name + ", Result: " + r.text)
-
-    @staticmethod
-    def agent_register_service(service, host, consul_url):
-        # payload = {
-        #             "ID": service.stack_name+'/'+service.name,
-        #             "Name": service.name,
-        #             "Tags": service.tags,
-        #             "Address": host.agent_ip,
-        #             "Port": int(host.port),
-        #             "Check": {
-        #                 "HTTP": "http://" + host.agent_ip + ":" + host.port + ""
-        #             }
-        #            }
-        services_id = []
-        payloads = ConsulRequest.generate_service_payload(service, host)
-        url = consul_url + "/v1/agent/service/register"
-        for payload in payloads:
-            r = requests.post(url, json=payload)
-            service.print_service()
-            services_id.append(payload["ID"])
-            print("Agent Register Service: " + service.stack_name+'/'+service.name + ", Result: " + r.text)
-        return services_id
-
-    @staticmethod
     def agent_register_container(container, host, consul_url):
         container_ids = []
         payloads = ConsulRequest.generate_container_payload(container, host)
@@ -58,58 +17,11 @@ class ConsulRequest:
         print(container_ids)
         return container_ids
 
-
-    @staticmethod
-    def remote_deregister_service(service_id, host, consul_url):
-        payload = {"Datacenter": host.dc,
-                   "Node": host.name,
-                   "ServiceID": service_id}
-        url = consul_url + "/v1/catalog/deregister"
-        r = requests.post(url, json=payload)
-        print("Remote Deregister Service: " + service_id + ", Result: " + r.text)
-
     @staticmethod
     def agent_deregister_service(service_id, consul_url):
         url = consul_url + "/v1/agent/service/deregister/"+service_id
         r = requests.post(url)
         print("Agent Deregister Service: " + service_id + ", Result: " + r.text)
-
-    @staticmethod
-    def generate_service_payload(service, host):
-        payloads = []
-        for i in service.tcp_port:
-            port = i.replace("prt:", "")
-            tmp = {
-                "ID": service.stack_name+'/'+service.name+"_" + port,
-                "Name": service.name+"_"+port,
-                "Tags": [i],
-                "Address": host.agent_ip,
-                "Port": int(port),
-            }
-            payloads.append(tmp)
-        if service.location:
-            for i in service.location:
-                tmp = {
-                    "ID": service.stack_name+'/'+service.name,
-                    "Name": service.name,
-                    "Tags": [],
-                    "Address": host.agent_ip,
-                    "Port": int(host.port),
-                    "Check": {}
-                }
-                loc = i.replace("loc:", "")
-                provide_location = loc.split(":")
-                tmp["Port"] = int(provide_location[0]) if len(provide_location) == 2 else tmp["Port"]
-                path = "".join(provide_location) if len(provide_location) == 2 else tmp["Port"]+loc
-                tmp["ID"] = tmp["ID"] + "_" + path
-                tmp["Name"] = tmp["Name"] + "_" + path
-                tmp["Tags"] = [i]
-                tmp["Check"] = {
-                    "HTTP": "http://"+host.agent_ip+":"+path,
-                    "Interval": "10s"
-                }
-                payloads.append(tmp)
-        return payloads
 
     @staticmethod
     def generate_container_payload(container, host):
--- metadatarequest.py
+++ metadatarequest.py
@@ -1,8 +1,7 @@
 import requests
 import host
-import service
-import stack
 import container
+
 
 class MetadataRequest:
     @staticmethod
@@ -20,111 +19,6 @@ class MetadataRequest:
         tmp_host.labels = res['labels']
         tmp_host.print_host()
         return tmp_host
-
-
-
-    @staticmethod
-    def get_self_service():
-        try:
-            res = requests.get(url="http://rancher-metadata/2015-07-25/self/service",
-                               headers={"Accept": "application/json"})
-        except requests.HTTPError:
-            print("HTTPError: get self service")
-            return None
-        res = res.json()
-        tmp_service = service.Service()
-        tmp_service.name = res['name']
-        tmp_service.hostname = res['hostname']
-        tmp_service.stack_name = res['stack_name']
-        tmp_service.ports = res['ports']
-        tmp_service.labels = res['labels']
-        #tmp_service.links = res['links']
-        for k, v in res['links'].items():
-            tmp_service.links.append(k.split("/")[1])
-
-        return tmp_service
-
-    @staticmethod
-    def get_self_stack():
-        try:
-            res = requests.get(url="http://rancher-metadata/2015-07-25/self/stack",
-                               headers={"Accept": "application/json"})
-        except requests.HTTPError:
-            print("HTTPError: get self stack")
-            return None
-        res = res.json()
-        tmp_stack = stack.Stack()
-        tmp_stack.name = res['name']
-        for i in res['services']:
-            tmp_stack.services.append(i)
-
-        return tmp_stack
-
-    @staticmethod
-    def get_other_service(name):
-        try:
-            res = requests.get(url="http://rancher-metadata/2015-07-25/services/"+name,
-                               headers={"Accept": "application/json"})
-        except requests.HTTPError:
-            print("HTTPError: get other service")
-            return None
-        res = res.json()
-        try:
-            if res["code"] == 404:
-                return None
-        except KeyError:
-            pass
-        tmp_service = service.Service()
-        tmp_service.name = res['name']
-        tmp_service.hostname = res['hostname']
-        tmp_service.stack_name = res['stack_name']
-        tmp_service.ports = res['ports']
-        tmp_service.labels = res['labels']
-        #tmp_service.links = res['links']
-        for k, v in res['links'].items():
-            tmp_service.links.append(k.split("/")[1])
-
-        return tmp_service
-
-    @staticmethod
-    def get_all_services():
-        try:
-            res = requests.get(url="http://rancher-metadata/2015-07-25/services",
-                               headers={"Accept": "application/json"})
-        except requests.HTTPError:
-            print("HTTPError: get all services")
-            return []
-
-        res = res.json()
-        print(res)
-        try:
-            if res["code"] == 404:
-                return []
-        except KeyError:
-            pass
-        tmp_services = []
-        for i in res:
-            tmp_service = service.Service()
-            tmp_service.name = i['name']
-            tmp_service.hostname = i['hostname']
-            tmp_service.stack_name = i['stack_name']
-            tmp_service.ports = i['ports']
-            tmp_service.labels = i['labels']
-
-            for prt in tmp_service.ports:
-                p = prt.split("/")
-                if len(p) > 1 and p[1] == 'tcp':
-                    tmp_service.tcp_ports.append(p[0].split(":")[0])
-                else:
-                    tmp_service.public_ports.append(p[0].split(":")[0])
-
-            try:
-                for loc in tmp_service.labels["location"]:
-                    tmp_service.location.append(loc)
-            except KeyError:
-                pass
-            tmp_services.append(tmp_service)
-        return tmp_services
 
     @staticmethod
     def get_all_containers():
